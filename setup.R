04defec0fc59cfef3bd7fb46c841ed4fc8301567
Taavi Päll <tapa741@gmail.com>
2015-06-08 11:16:30 +0300

    Initial setup and dir traverse

---
+++ setup.R
@@ -0,0 +1,6 @@
+setwd("Documents/TCGA-Assembler/")
+list.files()
+source("Module_A.r")
+source("Module_B.r")
+# TraverseAllDirectories(entryPoint = "https://tcga-data.nci.nih.gov/tcgafiles/ftp_auth/distro_ftpusers/anonymous/tumor/", 
+#                        fileLabel = "DirectoryTraverseResult")
